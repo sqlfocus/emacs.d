eca1e2a42d373478f3232e3849ba73f9dcc10ae3
lisongqing <lisongqing@126.com>
2016-07-19 08:54:33 +0800

    调整配色方案

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -1,16 +1,26 @@
 ;;; zenburn颜色方案
-;(require-package 'zenburn-theme)
-;(require 'zenburn-theme)
+; change
+;     `(helm-selection ((t (:background ,zenburn-bg+1 :underline nil))))
+;     `(helm-selection-line ((t (:background ,zenburn-bg+1))))
+; to
+;     `(helm-selection ((t (:foreground ,zenburn-bg :background ,zenburn-yellow-2))))
+;     `(helm-selection-line ((t (:foreground ,zenburn-bg :background ,zenburn-yellow-2))))
+(require-package 'zenburn-theme)
+(require 'zenburn-theme)
 
 ;;; solarized颜色方案
-(require-package 'color-theme)
-(require 'color-theme)
-(require-package 'color-theme-solarized)
-(require 'color-theme-solarized)
+;(require-package 'color-theme)
+;(require 'color-theme)
+;(require-package 'color-theme-solarized)
+;(require 'color-theme-solarized)
+;(load-theme 'solarized t)
+;(set-frame-parameter nil 'background-mode 'dark)
+;(enable-theme 'solarized)
 
-(load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)
-(enable-theme 'solarized)
+;;; spacemacs
+;(require-package 'spacemacs-theme)
+;(require 'spacemacs-common)
+;(load-theme 'spacemacs-dark t)
 
 ;;; provider
 (provide 'init-color-theme)
