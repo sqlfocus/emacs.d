681824a770933cce0cfbf134347aab81ee4c165a
lisongqing <lisongqing@126.com>
2016-07-19 22:20:29 +0800

    修正company配置

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,7 +1,21 @@
+;;; company
+;(require-package 'company)
+;(require 'company)
+
+;(add-hook 'after-init-hook 'global-company-mode)
+;(define-key company-mode-map (kbd "TAB") nil)
+;(define-key company-mode-map (kbd "M-/") 'company-complete-common)
+
+;;; company-helm
 (require-package 'company)
+(require-package 'helm-company)
 (require 'company)
+(require 'helm-company)
 
 (add-hook 'after-init-hook 'global-company-mode)
-(global-set-key "\t" 'company-complete-common)
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "M-/") 'helm-company)
+     (define-key company-active-map (kbd "M-/") 'helm-company)))
 
 (provide 'init-company)
