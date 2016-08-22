5835b17acfc74d586ee38014af97a1937173ccba
lisongqing <lisongqing@126.com>
2016-08-22 17:04:33 +0800

    在终端命令行下，关联x剪切板和emacs

--- custom.el
+++ custom.el
@@ -110,7 +110,6 @@
 (setq global-visual-line-mode t)               ; 激活visual line操作
 
 ;;;; Other
-(setq x-select-enable-clipboard t)             ; 把x的剪切板和emacs联系起来
 (setq Make-backup-files nil)                   ; No backup files ~
 (show-paren-mode 1)                            ; Highlight parenthesis pairs
 (setq pop-up-frame t)                          ; Buffers in separate frames
@@ -125,6 +124,44 @@
 (if (version< emacs-version "24.4")            ; 换行自动缩进
     (global-set-key (kbd "<RET>") 'newline-and-indent))
 
+;;;; 把x的剪切板和emacs联系起来(linux需要安装xsel, sudo apt-get install xsel)
+;; http://hugoheden.wordpress.com/2009/03/08/copypaste-with-emacs-in-terminal/
+;; I prefer using the "clipboard" selection (the one the
+;; typically is used by c-c/c-v) before the primary selection
+;; (that uses mouse-select/middle-button-click)
+(setq x-select-enable-clipboard t)
+
+;; If emacs is run in a terminal, the clipboard- functions have no
+;; effect. Instead, we use of xsel, see
+;; http://www.vergenet.net/~conrad/software/xsel/ -- "a command-line
+;; program for getting and setting the contents of the X selection"
+(unless window-system
+ (when (getenv "DISPLAY")
+  ;; Callback for when user cuts
+  (defun xsel-cut-function (text &optional push)
+    ;; Insert text to temp-buffer, and "send" content to xsel stdin
+    (with-temp-buffer
+      (insert text)
+      ;; I prefer using the "clipboard" selection (the one the
+      ;; typically is used by c-c/c-v) before the primary selection
+      ;; (that uses mouse-select/middle-button-click)
+      (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+  ;; Call back for when user pastes
+  (defun xsel-paste-function()
+    ;; Find out what is current selection by xsel. If it is different
+    ;; from the top of the kill-ring (car kill-ring), then return
+    ;; it. Else, nil is returned, so whatever is in the top of the
+    ;; kill-ring will be used.
+    (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+      (unless (string= (car kill-ring) xsel-output)
+	xsel-output )))
+  ;; Attach callbacks to hooks
+  (setq interprogram-cut-function 'xsel-cut-function)
+  (setq interprogram-paste-function 'xsel-paste-function)
+  ;; Idea from
+  ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+  ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+ ))
 
 (provide 'custom)
 
