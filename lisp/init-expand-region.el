02fe950408bc3ef8d39357e28a1cb8d6e7f12f85
lisongqing <lisongqing@126.com>
2016-08-24 22:11:39 +0800

    修正expand-region快捷键

--- lisp/init-expand-region.el
+++ lisp/init-expand-region.el
@@ -1,6 +1,8 @@
 (require-package 'expand-region)
 (require 'expand-region)
 
-(global-set-key (kbd "C-=") 'er/expand-region)
+;;;; 设置快捷键，C-=总是不生效，简化为＝
+;(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "=") 'er/expand-region)
 
 (provide 'init-expand-region)
